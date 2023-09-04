672d0a6197b9bf35c069b74fae317ce21dfa6acf
KULANA THATHSARA <100261303+it21219566@users.noreply.github.com>
2023-09-04 19:55:02 +0530

    Update pm.py

--- pm.py
+++ pm.py
@@ -1,5 +1,4 @@
 #Core Package
-from random import choice
 import streamlit as st
 
 #EDA Packages
@@ -11,18 +10,7 @@ def main():
     menu = ["Add New Item","Item List","Update","Delete","About"]
     choice = st.sidebar.selectbox("Menu",menu)
 
-    if choice == "Add New Item":
-        st.subheadder("New Item")
+    
 
-    elif choice == "Item List":
-        st.subheader("View Items")
-    elif choice == "Update":
-        st.subheader("Update Item")
-    elif choice == "Delete":
-        st.subheader("Delete Item")
-        
-    else:
-        st.subheader("About")
-
-if __name__ == ' main ':
+if __name__ == '_main_':
     main()
