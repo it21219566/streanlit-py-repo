83674f8606288d273fc24e6126fbba3437fee412
KULANA THATHSARA <100261303+it21219566@users.noreply.github.com>
2023-09-04 20:03:10 +0530

    Update pm.py

--- pm.py
+++ pm.py
@@ -1,27 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('100 Days of Python with Streamlit!')
-st.write('Welcome to my interactive website built using Streamlit and Python!')
+st.title('Add Item')
 
-def main():
-    st.title("Add Item")
-
-    menu = ["Add New Item","Item List","Update","Delete","About"]
-    choice = st.sidebar.selectbox("Menu",menu)
-
-    if choice == "Add New Item":
-        st.subheadder("New Item")
-
-    elif choice == "Item List":
-        st.subheader("View Items")
-    elif choice == "Update":
-        st.subheader("Update Item")
-    elif choice == "Delete":
-        st.subheader("Delete Item")
-        
-    else:
-        st.subheader("About")
-
-if __name__ == ' main ':
-    main()
+menu = ["Add New Item","Item List","Update","Delete","About"]
+choice = st.sidebar.selectbox("Menu",menu)
