80fb9e3524dd596ab68640ee107c2d92d06bd0ce
KULANA THATHSARA <100261303+it21219566@users.noreply.github.com>
2023-09-04 19:58:24 +0530

    Update pm.py

--- pm.py
+++ pm.py
@@ -1,16 +1,4 @@
-#Core Package
 import streamlit as st
 
-#EDA Packages
-import pandas as pd
-
-def main():
-    st.title("Add Item")
-
-    menu = ["Add New Item","Item List","Update","Delete","About"]
-    choice = st.sidebar.selectbox("Menu",menu)
-
-    
-
-if __name__ == '_main_':
-    main()
+st.title('100 Days of Python with Streamlit!')
+st.write('Welcome to my interactive website built using Streamlit and Python!')
