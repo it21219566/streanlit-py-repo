97f972e309f6365bd5f4887d2dd1f4b88f1f099a
KULANA THATHSARA <100261303+it21219566@users.noreply.github.com>
2023-09-04 20:44:59 +0530

    Update pm.py

--- pm.py
+++ pm.py
@@ -2,20 +2,4 @@ import streamlit as st
 import pandas as pd
 from random import choice
 
-st.title('Add Item')
-
-menu = ["Add New Item","Item List","Update","Delete","About"]
-choice = st.sidebar.selectbox("Menu",menu)
-
-if choice == "Add New Item":
-        st.subheadder("New Item")
-
-elif choice == "Item List":
-        st.subheader("View Items")
-elif choice == "Update":
-        st.subheader("Update Item")
-elif choice == "Delete":
-        st.subheader("Delete Item")
-        
-else:
-        st.subheader("About")
+st.title('Cart')
