92d08459f51ea30c814cb764c25e67d8e379ed8c
KULANA THATHSARA <100261303+it21219566@users.noreply.github.com>
2023-09-04 20:03:46 +0530

    Update pm.py

--- pm.py
+++ pm.py
@@ -5,3 +5,16 @@ st.title('Add Item')
 
 menu = ["Add New Item","Item List","Update","Delete","About"]
 choice = st.sidebar.selectbox("Menu",menu)
+
+if choice == "Add New Item":
+        st.subheadder("New Item")
+
+    elif choice == "Item List":
+        st.subheader("View Items")
+    elif choice == "Update":
+        st.subheader("Update Item")
+    elif choice == "Delete":
+        st.subheader("Delete Item")
+        
+    else:
+        st.subheader("About")
