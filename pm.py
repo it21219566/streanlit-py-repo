c0f43b1145197d042a49e3f1a7d3e46c683d92d6
KULANA THATHSARA <100261303+it21219566@users.noreply.github.com>
2023-09-04 19:59:41 +0530

    Update pm.py

--- pm.py
+++ pm.py
@@ -2,3 +2,22 @@ import streamlit as st
 
 st.title('100 Days of Python with Streamlit!')
 st.write('Welcome to my interactive website built using Streamlit and Python!')
+
+def main():
+    st.title("Add Item")
+
+    menu = ["Add New Item","Item List","Update","Delete","About"]
+    choice = st.sidebar.selectbox("Menu",menu)
+
+    if choice == "Add New Item":
+        st.subheadder("New Item")
+
+    elif choice == "Item List":
+        st.subheader("View Items")
+    elif choice == "Update":
+        st.subheader("Update Item")
+    elif choice == "Delete":
+        st.subheader("Delete Item")
+        
+    else:
+        st.subheader("About")
